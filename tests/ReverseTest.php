12ea00d5bf676aa837cd714988a91c75cf1f117d
Dan Mattern <dan@danmattern.com>
2018-03-23 18:42:10 -0400

    Uncommenting test, code coverage issue resolved.

--- tests/ReverseTest.php
+++ tests/ReverseTest.php
@@ -8,7 +8,7 @@ final class ReverseTest extends TestCase {
 	/**
 	 * @throws \Interview\Solutions\Strings\Reverse\Exceptions\InvalidAlgorithmException
 	 */
-	/*public function testDoesStringReverse(): void {
+	public function testDoesStringReverse(): void {
 		$chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 		$chars_array = str_split( $chars );
 		$length = 10;
@@ -17,7 +17,7 @@ final class ReverseTest extends TestCase {
 			$string .= array_rand( $chars_array );
 		}
 		$this->assertEquals( strrev( $string ), Reverse::reverseString( $string ) );
-	}*/
+	}
 
 	public function testBadInputProcess(): void {
 		$this->expectException( UnexpectedValueException::class );
