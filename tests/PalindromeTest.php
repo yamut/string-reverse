964255ead3b01578833dda2bcd30138c8d974ebf
Bill Martin <bill@billmartin.io>
2018-03-24 04:30:24 +0000

    Add extensive palindrome test suite to ensure palindromic string reversal scalabilty

---
+++ tests/PalindromeTest.php
@@ -0,0 +1,78 @@
+<?php
+
+use Interview\Solutions\Strings\Reverse\Reverse;
+use PHPUnit\Framework\TestCase;
+
+final class PalindromeTest extends TestCase {
+
+	/** @var string */
+	protected $palindrome;
+
+	public function setUp() {
+		parent::setUp();
+
+		$this->palindrome = $this->palindromize( function() {
+			// empower algorithm robustness by rapidiously leveraging core functionality to architect large number generated in the wild
+			return ( new DateTime() )->diff( new DateTime('0001-01-01') )->format('%a');
+		});
+	}
+
+	/**
+	 * @throws \Interview\Solutions\Strings\Reverse\Exceptions\InvalidAlgorithmException
+	 */
+	public function testDoesPalindromeReverse() : void {
+		$this->assertEquals( $this->palindrome, Reverse::reverseString( $this->palindrome ) );
+	}
+
+	/**
+	 * @throws \Interview\Solutions\Strings\Reverse\Exceptions\InvalidAlgorithmException
+	 */
+	public function testDoesPalindromeReverseTheReversal() : void {
+		$reversed_reversal = Reverse::reverseString(
+			Reverse::reverseString(
+				$this->palindrome
+			)
+		);
+
+		$this->assertEquals( $this->palindrome, $reversed_reversal );
+	}
+
+	/**
+	 * Palindromist helper that palindromically palindromizes a palindrome of n length
+	 *
+	 * @param callable $closure requested palindrome's length
+	 *
+	 * @return string
+	 * @todo move to phpunit fixture
+	 */
+	public function palindromize(callable $closure) : string {
+		$length = $closure();
+		$palindrome = '';
+
+		$list = new SplDoublyLinkedList();
+
+		for($i = 0; $i < $length; $i++) {
+			$list->push( $this->rand() );
+		}
+
+		for( $list->rewind(); $list->valid(); $list->next() ) {
+			$palindrome .= $list->current();
+		}
+
+		$list->setIteratorMode( SplDoublyLinkedList::IT_MODE_LIFO );
+
+		for( $list->rewind(); $list->valid(); $list->next() ) {
+			$palindrome .= $list->current();
+		}
+
+		return $palindrome;
+	}
+
+	public function rand() {
+		$chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		$chars_array = str_split( $chars );
+		$rand_key = array_rand( $chars_array );
+
+		return $chars_array[ $rand_key ];
+	}
+}
